2cfcf12adb529f23880ec0dd53841b9e38f431ec
Anshul Sharma <optimisticanshul@gmail.com>
2015-10-09 06:55:38 +0530

    Activate curse - Smelly Approach

--- lib/observer_pattern.rb
+++ lib/observer_pattern.rb
@@ -1,16 +1,28 @@
 class Tile
   def initialize(attrs = {})
     @cursed = attrs.fetch(:cursed, false)
+    @hero   = attrs.fetch(:hero, nil)
   end
 
   def cursed?
     @cursed
   end
+
+  def activate_curse
+    @hero.damage(4)
+  end
 end
 
 class Hero
+  attr_reader :health
+
   def initialize
     @cursed = false
+    @health = 10
+  end
+
+  def damage(hit)
+    @health -= hit
   end
 
   def cursed?
@@ -18,6 +30,6 @@ class Hero
   end
 
   def discover(tile)
-    @cursed = tile.cursed?
+    @cursed = true if tile.cursed?
   end
 end
--- spec/observer_pattern_spec.rb
+++ spec/observer_pattern_spec.rb
@@ -5,8 +5,9 @@ require_relative '../lib/observer_pattern'
 describe "Observer Pattern" do
 
   describe Hero do
+    let (:hero) { Hero.new }
+
     it "is cursed when discover cursed tile" do
-      hero = Hero.new
       tile = Tile.new cursed: true
 
       hero.discover(tile)
@@ -15,16 +16,47 @@ describe "Observer Pattern" do
     end
 
     it "is not cursed when discover simple Tile without cursed" do
-      hero = Hero.new
       tile = Tile.new cursed: false
 
       hero.discover(tile)
 
       expect(hero.cursed?).to be_falsey
     end
+
+    it "has default health to 10" do
+      expect(hero.health).to eq(10)
+    end
+
+    it "can be damaged" do
+      hero.damage(6)
+      expect(hero.health).to eq(4)
+    end
   end
 
   describe Tile do
+
+    it "activates curse" do
+      hero = Hero.new
+      tile = Tile.new cursed: true, hero: hero
+
+      tile.activate_curse
+      expect(hero.health).to eq(6)
+    end
+
+    it "activates curse on several heros" do
+      hero1 = Hero.new
+      hero2 = Hero.new
+      tile  = Tile.new
+
+      hero1.discover(tile)
+      hero2.discover(tile)
+
+      tile.activate_curse
+
+      expect(hero1.health).to eq (6)
+      expect(hero2.health).to eq (6)
+    end
+
     it "is not cursed by default" do
       tile = Tile.new
       expect(tile.cursed?).to be_falsey
