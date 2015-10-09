c6e0ee0b1115a91acf31fdc1abbfa46d4bd19d1d
Anshul Sharma <optimisticanshul@gmail.com>
2015-10-09 10:00:50 +0530

    External Iterator

--- lib/iterator.rb
+++ lib/iterator.rb
@@ -1,11 +1,37 @@
 class Inventory
+  attr_reader :items
 
+  def initialize
+    @items = []
+  end
+
+  def add(item)
+    @items << item
+  end
 end
 
 class Item
-  attr_reader :cost
+  attr_accessor :cost
 
   def initialize
     @cost = 0
+  end
+end
+
+
+class InventoryIterator
+  def initialize(inventory)
+    @items = inventory.items
+    @index = 0
+  end
+
+  def has_next?
+    @index < @items.size
+  end
+
+  def next
+    value = @items[@index]
+    @index += 1
+    value
   end
 end
--- spec/iterator_spec.rb
+++ spec/iterator_spec.rb
@@ -1,10 +1,35 @@
-require_relative './spec_helper'
+require 'spec_helper'
 
 require_relative '../lib/iterator'
 
 describe 'Iterator Pattern' do
 
   describe 'Inventory' do
+    let(:inventory) { Inventory.new }
+    before :each do
+      item1 = Item.new
+      item1.cost = 20
+
+      item2 = Item.new
+      item2.cost = 10
+
+      inventory.add(item1)
+      inventory.add(item2)
+    end
+
+    it 'can be iterated through items to get total cost' do
+      # result = inventory.items.inject(0) { |sum, item| sum + item.cost}
+      iterator = InventoryIterator.new(inventory)
+      result = 0
+      while iterator.has_next?
+        result += iterator.next.cost
+      end
+      expect(result).to eq(30)
+    end
+
+    it 'adds items to its collection' do
+      expect(inventory.items.size).to eq(2)
+    end
 
   end
 
