3c145e110cc687d922d0e4f4949c7870888bc558
Anshul Sharma <optimisticanshul@gmail.com>
2015-10-09 18:05:35 +0530

    Abstract Factory Pattern

--- creational_patterns/spec/factory_spec.rb
+++ creational_patterns/spec/factory_spec.rb
@@ -3,13 +3,23 @@ require_relative '../lib/factory'
 
 describe 'Factory Pattern' do
 
+  let(:party) { Party.new(HeroFactory.new) }
+
   it 'has party of warriors' do
-    party = PartyFactory.new(3, :warrior)
+    party.add_warriors(3)
     expect(party.members.count { |member| member.class == Warrior}).to eq(3)
   end
 
   it 'has party of mages' do
-    party = PartyFactory.new(3, :mage)
+    party.add_mages(3)
     expect(party.members.count { |member| member.class == Mage}).to eq(3)
+  end
+
+  it 'mixed party' do
+    party.add_mages(2)
+    party.add_warriors(1)
+    expect(party.members.count { |member| member.class == Mage}).to eq(2)
+    expect(party.members.count { |member| member.class == Warrior}).to eq(1)
+    expect(party.members.count).to eq(3)
   end
 end
